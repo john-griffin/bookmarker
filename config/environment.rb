a0fefefdb09c3d7ca0d211442a4a5c715e7a6d3b
John Griffin <johnog@gmail.com>
2010-02-26 22:45:10 +0000

    added compass and sites controller

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,8 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'compass'
+  config.gem 'haml'
   config.gem "bitly"
-  config.gem "haml"
   config.time_zone = 'UTC'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
+  
+  map.resources :sites do |sites|
+    sites.resources :bookmarks
+  end
 
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
