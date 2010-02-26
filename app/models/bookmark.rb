2286b98d4550efde97ebb4fba1a2081f18c49230
John Griffin <johnog@gmail.com>
2010-02-26 19:03:19 +0000

    make bookmarks taggable

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,5 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :site
+  
+  acts_as_taggable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100226140147) do
+ActiveRecord::Schema.define(:version => 20100226190112) do
 
   create_table "bookmarks", :force => true do |t|
     t.string   "url"
@@ -24,6 +24,20 @@ ActiveRecord::Schema.define(:version => 20100226140147) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
   end
 
 end
