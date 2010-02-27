4f2cdecdcee6d85aa26f96dfa477c5a04542e0fc
John Griffin <johnog@gmail.com>
2010-02-27 11:08:38 +0000

    use correct version of compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,7 @@
 require 'compass'
+
   # If you have any compass plugins, require them here.
-  Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.rb"))
+  Compass.add_project_configuration(File.join(RAILS_ROOT, "config", "compass.rb"))
   Compass.configuration.environment = (defined?(Rails) ? Rails.env : RAILS_ENV).to_sym
   Compass.configure_sass_plugin!
   
