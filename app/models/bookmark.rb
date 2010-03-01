06a4e64ac41231189c42dffedf4e12918644f75a
John Griffin <johnog@gmail.com>
2010-03-01 14:21:27 +0000

    improve url validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,6 +5,7 @@ class Bookmark < ActiveRecord::Base
   
   validates_presence_of :url, :on => :create, :message => "must be entered"
   validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix, :message => "must be valid"
+  validate :url_must_exist, :if => :valid_url
   
   def before_create
     attach_site!
@@ -13,11 +14,15 @@ class Bookmark < ActiveRecord::Base
   end
   
   private
-  def update_title!
+  
+  def get_page(url)
     a = Mechanize.new { |agent|
       agent.user_agent_alias = 'Mac Safari'
-    }
-    self.title = a.get(url).title || url
+    }.get(url)
+  end
+  
+  def update_title!
+    self.title = get_page(url).title || url
   end
   
   def attach_site!
@@ -33,6 +38,18 @@ class Bookmark < ActiveRecord::Base
   def update_url_short!
     bitly = Bitly.new("splitstate", "R_c8f065fed0cdeec065d93c9d55823acc")
     self.url_short = bitly.shorten(url).short_url
+  end
+  
+  def valid_url
+    self.errors.empty?
+  end
+  
+  def url_must_exist
+    begin
+      get_page(url)
+    rescue SocketError => e
+      errors.add(:url, " must be valid")
+    end
   end
   
 end
